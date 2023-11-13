b76331cbc680e8f2ac919714fef0c650165a5376
Kevin Nicholas <kevinnicholas2019@gmail.com>
2023-11-13 17:32:57 +0700

    asd

--- template-parts/splide-content-header-attach-fade-vr.php
+++ template-parts/splide-content-header-attach-fade-vr.php
@@ -1,4 +1,4 @@
-<div class="splide splide-content-header-attach-fade-vr position-absolute w-100 h-100" style="left: 0; top: 0; z-index: -1;">
+<div class="splide splide-content-header-attach-fade-vr position-absolute w-100 h-100" style="left: 0; top: 0;">
     <div class="splide__track position-absolute w-100 h-100" style="top: 0; z-index: -1;">
         <ul class="splide__list">
             <?php for ($i = 0; $i < 3; $i++) { ?>
