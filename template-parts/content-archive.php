5a0f1c74aace7e4031ef05ca217e742330cb82f6
Kevin Nicholas <kevinnicholas2019@gmail.com>
2023-11-17 02:25:28 +0700

    day 6

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -4,7 +4,7 @@
             <img class="me-md-3 mb-md-0 mb-3 img-fluid post-thumb" src="<?= the_post_thumbnail_url() ?>" alt="image">
             <div class="media-body">
                 <h3 class="title mb-1"><?= the_title() ?></h3>
-                <div class="meta mb-1"><span class="date"><?= the_date() ?></span><span class="comment"><a href="#"><?= get_comments_number_text() ?></a></span></div>
+                <div class="meta mb-1"><span class="date"><?= the_date() ?></span><span class="ms-2 me-2">|</span><i class="fas fa-comment me-1"></i> <span class="comment"><a href="#"><?= get_comments_number_text() ?></a></span></div>
                 <div class="intro"><?= the_excerpt() ?></div>
                 <a class="more-link" href="<?= the_permalink() ?>">Read more &rarr;</a>
             </div><!--//media-body-->
